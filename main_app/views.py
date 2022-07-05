cb10c983d13e424e809ae44a61b69cbe7e4ecf54
Peter Yoo <trustintofu@gmail.com>
2022-07-05 13:20:01 -0700

    wip: imports Finch model to admin portal

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-class Finches:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-finches = [
-  Finches('Mask', 'American Goldfinch', 'loves sunflowers and nujer', 1),
-  Finches('Hot Cheeto', 'Cassia Crossbill', 'dangles from cones', 0),
-  Finches('Mustang', 'Evening Grosbeak', 'prefers platform feeders', 2),
-]
+from .models import Finch
 
 # Create your views here.
 def home(request):
@@ -22,4 +10,5 @@ def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', { 'finches': finches })
